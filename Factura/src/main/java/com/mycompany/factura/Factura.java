b7cdad61c47de621eaea7c0e59218b456cfff47e
EstebanZ898 <estebanzamora4toc@gmail.com>
2024-10-03 14:26:49 -0600

    Add files via upload

---
+++ Factura/src/main/java/com/mycompany/factura/Factura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.factura;
+
+/**
+ *
+ * @author esteb
+ */
+public class Factura {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

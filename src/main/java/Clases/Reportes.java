45004ad7b545646b0d2aaa42abbeda29dd03e25b
d4vidmonte472 <161400702+d4vidmonte472@users.noreply.github.com>
2024-10-28 19:41:02 -0600

    ?

--- src/main/java/Clases/Reportes.java
+++ src/main/java/Clases/Reportes.java
@@ -12,13 +12,15 @@ public class Reportes {
     
     
     private int numQueja;
+    private String fechaQueja;
     private String nombreConsumidor;
     private String nombreEmpresa;
     private String detalle;
     private String solicitud;
 
-    public Reportes(int numQueja, String nombreConsumidor, String nombreEmpresa, String detalle, String solicitud) {
+    public Reportes(int numQueja, String fechaQueja, String nombreConsumidor, String nombreEmpresa, String detalle, String solicitud) {
         this.numQueja = numQueja;
+        this.fechaQueja = fechaQueja;
         this.nombreConsumidor = nombreConsumidor;
         this.nombreEmpresa = nombreEmpresa;
         this.detalle = detalle;
@@ -27,6 +29,10 @@ public class Reportes {
 
     public int getNumQueja() {
         return numQueja;
+    }
+    
+    public String getFechaQueja(){
+        return fechaQueja;
     }
 
     public String getNombreConsumidor() {
@@ -47,6 +53,10 @@ public class Reportes {
 
     public void setNumQueja(int numQueja) {
         this.numQueja = numQueja;
+    }
+    
+    public void setFechaQueja(String fechaQueja){
+        this.fechaQueja = fechaQueja;
     }
 
     public void setNombreConsumidor(String nombreConsumidor) {
